3410f83b514c68e2344753c1d5d7176bb78d6bdb
Nagesh <nageshvnkt@gmail.com>
2023-12-20 16:31:01 +0530

    First version

---
+++ tns/src/main/java/com/example/tns/TnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnsApplication.class, args);
+	}
+
+}
---
+++ tns/src/test/java/com/example/tns/TnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
